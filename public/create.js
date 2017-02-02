268154e81b4ae27d677843b83b3fc07932a463ef
Kaito Udagawa <umireon@gmail.com>
2017-02-03 00:38:12 +0900

    Refactor create.js

--- public/create.js
+++ public/create.js
@@ -1,29 +1,18 @@
+/* eslint-env browser */
+/* global Vue, dialogPolyfill, firebase, moment */
+
 var config = {
-  apiKey: "AIzaSyB3rU05SgP6XFnQqPgrvCBLSPulxsfpwxI",
-  authDomain: "sns-taka3sh-org-157419.firebaseapp.com",
-  databaseURL: "https://sns-taka3sh-org-157419.firebaseio.com",
-  messagingSenderId: "895779023522"
+  apiKey: 'AIzaSyB3rU05SgP6XFnQqPgrvCBLSPulxsfpwxI',
+  authDomain: 'sns-taka3sh-org-157419.firebaseapp.com',
+  databaseURL: 'https://sns-taka3sh-org-157419.firebaseio.com'
 }
 firebase.initializeApp(config)
 var auth = firebase.auth()
 var database = firebase.database()
-var messaging = firebase.messaging()
 
-function notify(message) {
-  document.querySelector('.mdl-js-snackbar').MaterialSnackbar.showSnackbar({
-    message: message
-  });
-}
-
-function disableForm(form, disabled) {
-  form.querySelectorAll('input').forEach(function(elem) {
-    elem.disabled = disabled
-  })
-}
-
-function updateMDL() {
-  Vue.nextTick(function() {
-    document.querySelectorAll('.mdl-js-textfield').forEach(function(elem) {
+function updateMDL () {
+  Vue.nextTick(function () {
+    document.querySelectorAll('.mdl-js-textfield').forEach(function (elem) {
       if (elem.MaterialTextfield) {
         elem.MaterialTextfield.checkDirty()
       }
@@ -31,76 +20,107 @@ function updateMDL() {
   })
 }
 
-function onSubmit(e) {
-  disableForm(e.target, true)
-
-  var newPostKey = database.ref().child('posts').push().key
-  var updates = {}
-  updates['/posts/' + newPostKey] = {
-    title: this.title,
-    body: this.body,
-    createdAt: moment(this.createdAt).toISOString(),
-  }
-
-  database.ref().update(updates).then(() => {
-    disableForm(e.target, false)
-    e.target.reset()
-    notify("A new post was successfully created")
-  }).catch((error) => {
-    disableForm(e.target, false)
-    notify(error.message)
+function setDisabled (form, disabled) {
+  form.querySelectorAll('input, textarea').forEach(function (elem) {
+    elem.disabled = disabled
   })
 }
 
-function onReset() {
+function createPost (form) {
+  var newPostKey = database.ref().child('posts').push().key
+  var updates = {}
+  updates['/posts/' + newPostKey] = {
+    title: form.elements.title,
+    body: form.elements.body,
+    createdAt: moment(form.elements.createdAt).toISOString()
+  }
+  return database.ref().update(updates)
+}
+
+function publishNotification (form) {
+  return auth.currentUser.getToken(true)
+  .then(function (idToken) {
+    return fetch('https://sns-taka3sh-org-157419.appspot.com/publish', {
+      method: 'POST',
+      body: new FormData(form)
+    })
+  })
+}
+
+function showToast (message) {
+  document.querySelector('.mdl-js-snackbar').MaterialSnackbar.showSnackbar({
+    message: message
+  })
+}
+
+function onSubmit (e) {
+  setDisabled(e.target, true)
+
+  createPost(e.target)
+  .then(function () {
+    return publishNotification(e.target)
+  })
+  .then(function () {
+    e.target.reset()
+    setDisabled(e.target, false)
+    showToast('A new post was successfully created.')
+  })
+  .catch(function (error) {
+    setDisabled(e.target, false)
+    showToast(error.message)
+    console.log(error)
+  })
+}
+
+function onReset () {
   this.title = this.body = ''
   this.createdAt = moment().startOf('minute').format()
   updateMDL()
 }
 
-function onLogout(e) {
+function onLogout (e) {
   auth.signOut()
 }
 
 var app = new Vue({
   el: '#app',
   data: {
-    title: "",
-    body: "",
-    createdAt: "",
-    user: null,
+    title: '',
+    body: '',
+    createdAt: '',
+    user: null
   },
   methods: {
-    localizeDate: function(date) {
+    localizeDate: function (date) {
       return moment(date).format('LLLL')
     },
     onLogout: onLogout,
     onSubmit: onSubmit,
-    onReset: onReset,
+    onReset: onReset
   },
-  mounted: function() {
+  mounted: function () {
     document.forms.post.reset()
   }
 })
 
-/// Login form
+// Login form
 
-document.forms.login.addEventListener('submit', function(e) {
+document.forms.login.addEventListener('submit', function (e) {
   e.preventDefault()
-  disableForm(e.target, true)
+  setDisabled(e.target, true)
   var email = e.target.elements.email.value
   var password = e.target.elements.password.value
-  auth.signInWithEmailAndPassword(email, password).then(function() {
-    disableForm(e.target, false)
+  auth.signInWithEmailAndPassword(email, password).then(function () {
+    setDisabled(e.target, false)
     e.target.reset()
     updateMDL()
   }).catch(function (error) {
-    disableForm(e.target, false)
-    notify(error.message)
+    setDisabled(e.target, false)
+    showToast(error.message)
   })
 })
 
-document.getElementById('cancel').addEventListener('click', function(e) {
+document.getElementById('cancel').addEventListener('click', function (e) {
   e.preventDefault()
   history.back()
 })
@@ -108,11 +128,11 @@ document.getElementById('cancel').addEventListener('click', function(e) {
 var dialog = document.querySelector('dialog')
 dialogPolyfill.registerDialog(dialog)
 
-dialog.addEventListener('cancel', function(e) {
+dialog.addEventListener('cancel', function (e) {
   e.preventDefault()
 })
 
-auth.onAuthStateChanged(function(user) {
+auth.onAuthStateChanged(function (user) {
   if (user) {
     app.user = user.email
     dialog.close()
