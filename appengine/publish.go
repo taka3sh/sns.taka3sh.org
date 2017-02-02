f5e8c7dfc104a24ae54ccb11efcbdc34e02c33f0
Kaito Udagawa <umireon@gmail.com>
2017-02-03 03:24:39 +0900

    Add id verification

--- appengine/publish.go
+++ appengine/publish.go
@@ -2,13 +2,60 @@ package appengine
 
 import (
 	"bytes"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/urlfetch"
 )
+
+func fetchPublicKeys(client *http.Client) (map[string]string, error) {
+	resp, err := client.Get("https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com")
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var keys map[string]string
+	err = json.Unmarshal(data, &keys)
+	return keys, err
+}
+
+func validateUID(publicKeys map[string]string, idToken string) error {
+	_, err := jwt.Parse(idToken, func(token *jwt.Token) (interface{}, error) {
+		kid, ok := token.Header["kid"].(string)
+		if !ok {
+			return nil, errors.New("kid was not found")
+		}
+
+		certPEM, ok := publicKeys[kid]
+		if !ok {
+			return nil, errors.New("the corresponding public key was not found")
+		}
+
+		block, _ := pem.Decode([]byte(certPEM))
+
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return nil, err
+		}
+
+		return cert.PublicKey, nil
+	})
+
+	return err
+}
 
 func publish(client *http.Client, key string) (resp *http.Response, err error) {
 	jsonStr, _ := json.Marshal(map[string]interface{}{
@@ -37,6 +84,15 @@ func handlePublish(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
+	}
+
+	publicKeys, err := fetchPublicKeys(client)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	if validateUID(publicKeys, r.FormValue("idToken")) != nil {
+		http.Error(w, err.Error(), 403)
 	}
 
 	resp, err := publish(client, key)
