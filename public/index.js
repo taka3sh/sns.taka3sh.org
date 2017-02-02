97216efdc82c06a33d2e2fd4578da5c0ce4898a7
Kaito Udagawa <umireon@gmail.com>
2017-02-02 22:33:10 +0900

    Add messaging clients

--- public/index.js
+++ public/index.js
@@ -1,8 +1,12 @@
-const config = {
-  apiKey: "AIzaSyCRvuGgcUMJZdptmwAGighs6gU741NLQhs",
-  databaseURL: "https://sns-taka3sh-org.firebaseio.com",
-}
+firebase.initializeApp({
+  apiKey: 'AIzaSyCRvuGgcUMJZdptmwAGighs6gU741NLQhs',
+  databaseURL: 'https://sns-taka3sh-org.firebaseio.com',
+  messagingSenderId: '797345535544',
+})
+var database = firebase.database()
+var messaging = firebase.messaging()
 
+/// Application
 var app = new Vue({
   el: '#app',
   data: {
@@ -15,7 +19,38 @@ var app = new Vue({
   }
 })
 
-firebase.initializeApp(config)
-firebase.database().ref('posts').on('child_added', function(data) {
+database.ref('posts').on('child_added', function(data) {
   app.posts.unshift(data.val())
 })
+
+/// Messaging
+
+function sendTokenToServer(token) {
+  var endpoint = 'https://sns-taka3sh-org-157419.appspot.com/subscribe/' + token
+  fetch(endpoint, { method: "POST" })
+}
+
+messaging.onTokenRefresh(function() {
+  messaging.getToken()
+  .then(function(refreshedToken) {
+    sendTokenToServer(refreshedToken)
+  })
+})
+
+messaging.onMessage(function(payload) {
+  console.log(payload)
+})
+
+function requestPermission() {
+  messaging.requestPermission()
+  .then(function() {
+    messaging.getToken()
+    .then(function(currentToken) {
+      if (currentToken) {
+        sendTokenToServer(currentToken)
+      }
+    })
+  })
+}
+
+requestPermission()
