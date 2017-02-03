452f33be3e7328e72f2a4a895612d5ace091aecf
Kaito Udagawa <umireon@gmail.com>
2017-02-04 03:06:53 +0900

    Remove a message handler

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,15 +7,3 @@ firebase.initializeApp({
   'messagingSenderId': '797345535544'
 })
 var messaging = firebase.messaging()
-
-messaging.setBackgroundMessageHandler(function (payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload)
-  // Customize notification here
-  const notificationTitle = 'Background Message Title'
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  }
-
-  return self.registration.showNotification(notificationTitle, notificationOptions)
-})
