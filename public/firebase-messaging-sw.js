3fa679df612cbd115794fbeb98e014c10da5c2c0
Kaito Udagawa <umireon@gmail.com>
2017-02-04 03:13:03 +0900

    Refactor sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,7 @@
 /* eslint-env serviceworker */
 /* global firebase */
-
 importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js')
 firebase.initializeApp({
   'messagingSenderId': '797345535544'
 })
-var messaging = firebase.messaging()
