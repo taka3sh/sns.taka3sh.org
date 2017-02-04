77dca17b6e60eb24bf1cf5cffac9dc872b319971
Kaito Udagawa <umireon@gmail.com>
2017-02-04 19:09:14 +0900

    Remove fetch polyfill

--- public/create.js
+++ public/create.js
@@ -44,9 +44,12 @@ function publishNotification (form) {
   return auth.currentUser.getToken(true)
   .then(function (idToken) {
     form.elements.idToken.value = idToken
-    return fetch('https://sns-taka3sh-org-157419.appspot.com/publish', {
-      method: 'POST',
-      body: new FormData(document.forms[0])
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest()
+      xhr.onload = resolve
+      xhr.onerror = reject
+      xhr.open('POST', 'https://sns-taka3sh-org-157419.appspot.com/publish')
+      xhr.send(new FormData(form))
     })
   })
 }
