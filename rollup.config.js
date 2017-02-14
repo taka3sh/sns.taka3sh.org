ced1912e168c3748458e0a6d6cac600c60bcc2bb
Kaito Udagawa <umireon@gmail.com>
2017-02-14 15:26:18 +0900

    Add vue plugin to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,11 @@
+import vue from 'rollup-plugin-vue'
+
 export default {
   entry: './src/index.js',
   format: 'iife',
   moduleName: 'app',
-  dest: './www/index.js'
+  dest: './www/index.js',
+  plugins: [
+    vue
+  ]
 }
