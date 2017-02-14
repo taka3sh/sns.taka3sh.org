89eea1bab7fcd88cd1349ffbceef480b99d448cf
Kaito Udagawa <umireon@gmail.com>
2017-02-15 02:25:32 +0900

    Cache the logged-in user

--- src/AuthService.js
+++ src/AuthService.js
@@ -1,13 +1,25 @@
+/* globals localStorage */
+
 export default {
   init: function (auth) {
     this.auth = auth
   },
 
+  getUser: function () {
+    return localStorage.getItem('AuthService.user')
+  },
+
   login: function (email, password) {
     return this.auth.signInWithEmailAndPassword(email, password)
+    .then(function (user) {
+      localStorage.setItem('AuthService.user', user.email)
+    })
   },
 
   logout: function () {
     this.auth.signOut()
+    .then(function () {
+      localStorage.removeItem('AuthService.user')
+    })
   }
 }
