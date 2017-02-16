c0fd18c168d47bd84d70eae06ad1c0d56bb222fd
Kaito Udagawa <umireon@gmail.com>
2017-02-17 01:00:00 +0900

    Fix messagingSenderId

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,7 +4,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js')
 
 firebase.initializeApp({
-  'messagingSenderId': '797345535544'
+  'messagingSenderId': '895779023522'
 })
 
 firebase.messaging()
--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
 export var firebaseApiKey = 'AIzaSyB3rU05SgP6XFnQqPgrvCBLSPulxsfpwxI'
 export var firebaseAuthDomain = 'sns-taka3sh-org-157419.firebaseapp.com'
 export var firebaseDatabaseURL = 'https://sns-taka3sh-org-157419.firebaseio.com'
-export var firebaseMessagingSenderId = 'AIzaSyB3rU05SgP6XFnQqPgrvCBLSPulxsfpwxI'
+export var firebaseMessagingSenderId = '895779023522'
 
 export var notifyEndpoint = 'https://sns-taka3sh-org-157419.appspot.com/subscribe/'
 
