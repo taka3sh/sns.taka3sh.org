0c000fa7271f4de464d6ee2bd00a1b97d295a407
Kaito Udagawa <umireon@gmail.com>
2017-02-17 00:06:37 +0900

    Fix notifyEndpoint

--- src/Constants.js
+++ src/Constants.js
@@ -3,7 +3,7 @@ export var firebaseAuthDomain = 'sns-taka3sh-org-157419.firebaseapp.com'
 export var firebaseDatabaseURL = 'https://sns-taka3sh-org-157419.firebaseio.com'
 export var firebaseMessagingSenderId = 'AIzaSyB3rU05SgP6XFnQqPgrvCBLSPulxsfpwxI'
 
-export var notifyEndpoint = 'AIzaSyB3rU05SgP6XFnQqPgrvCBLSPulxsfpwxI'
+export var notifyEndpoint = 'https://sns-taka3sh-org-157419.appspot.com/subscribe/'
 
 export var pushEndpoint = 'https://sns-taka3sh-org-157419.appspot.com/publish'
 
