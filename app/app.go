b4264cd6ef4c5d27167cb5c0e1b21177803f63b7
Yudai Udagawa <nimius.rd@gmail.com>
2017-02-19 17:37:01 +0900

    Fix typo

--- app/app.go
+++ app/app.go
@@ -17,7 +17,7 @@ func init() {
 	http.Handle("/subscribe/", subscribeServer{topic: "/rel/topics/posts"})
 	http.Handle("/subscribe.dev/", subscribeServer{topic: "/rel/topics/posts.dev"})
 	http.Handle("/publish", publishServer{topic: "/topics/posts"})
-	http.Handle("/publish.dev", publishServer{topic: "/topics/posts-dev"})
+	http.Handle("/publish.dev", publishServer{topic: "/topics/posts.dev"})
 	http.HandleFunc("/index.html", handleIndex)
 }
 
