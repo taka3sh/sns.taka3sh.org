4ba81b875527b88e692acc1e6e9e4acd0ecf5cf4
Kaito Udagawa <umireon@gmail.com>
2017-02-21 01:03:06 +0900

    Adopt Materialize

--- src/partial/PostCards.vue
+++ src/partial/PostCards.vue
@@ -1,11 +1,9 @@
 <template>
   <transition-group name="card" tag="section" class="mdl-grid" v-show="ready">
-    <div v-for="post in posts" class="mdl-card mdl-cell mdl-cell--12-col mdl-shadow--2dp" :id="post.key" :key="post">
-      <div class="mdl-card__title">
-        <h2 class="mdl-card__title-text">{{ post.title }}</h2>
-      </div>
-      <div class="mdl-card__supporting-text">
-        <p v-for="line in post.body.split('\n')" v-text="line"></p>
+    <div class="card" v-for="post in posts" :id="post.key" :key="post">
+      <div class="card-title amber darken-1">{{ post.title }}</div>
+      <div class="card-content">
+        <p v-for="line in post.body.split('\n')">{{ line }}</p>
         <p>{{ post.createdAt | date-localize }}</p>
       </div>
     </div>
