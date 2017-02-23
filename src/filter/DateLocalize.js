99f981e74c5ea60d004d811d605796d9b03658ba
Kaito Udagawa <umireon@gmail.com>
2017-02-23 17:35:06 +0900

    Prevent a malformed date string from appearing

--- src/filter/DateLocalize.js
+++ src/filter/DateLocalize.js
@@ -1,7 +1,11 @@
-const days = '日月火水木金土'
+var days = '日月火水木金土'
 
 export default function (value) {
-  const date = new Date(value)
-  const hours = date.getHours()
-  return date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日' + (hours >= 12 ? '午前' : '午後') + ((hours + 11) % 12 + 1) + '時' + date.getMinutes() + '分 ' + days[date.getDay()] + '曜日'
+  var date = new Date(value)
+  if (isNaN(date.getTime())) {
+    return 'Invalid date'
+  } else {
+    var hours = date.getHours()
+    return date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日' + (hours >= 12 ? '午前' : '午後') + ((hours + 11) % 12 + 1) + '時' + date.getMinutes() + '分 ' + days[date.getDay()] + '曜日'
+  }
 }
