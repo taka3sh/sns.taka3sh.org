378bb39b04d24441b9cb063b760593ef765bb538
Kaito Udagawa <umireon@gmail.com>
2020-11-07 18:48:29 +0900

    Make index.html to be React

---
+++ src/constants/development.js
@@ -0,0 +1,14 @@
+export {
+  firebaseProjectId,
+  firebaseApiKey,
+  firebaseAuthDomain,
+  firebaseDatabaseURL,
+  firebaseMessagingSenderId,
+  firebaseAppId
+} from './production'
+
+export const notifyEndpoint = 'https://sns-taka3sh-org-157419.appspot.com/subscribe.dev'
+
+export const pushEndpoint = 'https://sns-taka3sh-org-157419.appspot.com/publish.dev'
+
+export const postPrefix = 'posts-stage'
