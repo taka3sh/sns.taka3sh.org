25b14c433da7bfd348e579bb108918634b748897
ahirucyann <ahirucyann@users.noreply.github.com>
2020-11-24 00:27:44 +0900

    Update IndexApp.tsx

--- src/IndexApp.tsx
+++ src/IndexApp.tsx
@@ -83,7 +83,7 @@ const IndexApp: React.FC<unknown> = () => {
     <div className="grey lighten-3">
       <nav className="pink darken-1">
         <div className="nav-wrapper container">
-          <span className="brand-logo">支援隊ヌーボー</span>
+          <span className="brand-logo" style="font-size: 1.7rem;">支援隊ヌーボー</span>
         </div>
       </nav>
 
