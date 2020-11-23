08740fcc0c025fd9cd77868b98ff307b5357edd9
Kaito Udagawa <umireon@gmail.com>
2020-11-24 01:13:29 +0900

    2020

--- src/IndexApp.tsx
+++ src/IndexApp.tsx
@@ -107,7 +107,7 @@ const IndexApp: React.FC<unknown> = () => {
 
       <footer className="page-footer grey darken-3 white-text">
         <div className="container">
-          <div>Copyright © 2015-2017 高井戸第三小学校学校支援本部</div>
+          <div>Copyright © 2015-2020 高井戸第三小学校学校支援本部</div>
           <div>
             Developed by
             <a href="https://github.com/umireon">umireon</a>
