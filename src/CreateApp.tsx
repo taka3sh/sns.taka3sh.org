e4f05140da3527d623e41105edf9a1b0c4b92bf1
ahirucyann <ahirucyann@users.noreply.github.com>
2020-11-24 00:40:11 +0900

    Update CreateApp.tsx

--- src/CreateApp.tsx
+++ src/CreateApp.tsx
@@ -118,7 +118,7 @@ const CreateApp: React.FC<unknown> = () => {
     <div className="grey lighten-3">
       <nav className="pink darken-1">
         <div className="nav-wrapper container">
-          <span className="brand-logo" style={{ fontSize: '1.7rem' }}>
+          <span className="brand-logo" style={{ fontSize: '1.5rem' }}>
             支援隊ヌーボー
           </span>
         </div>
