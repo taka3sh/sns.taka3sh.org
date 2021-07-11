04df366447153f5dff92b6fa4ff1965c21d9c02c
Kaito Udagawa <umireon@gmail.com>
2021-07-11 17:51:18 +0900

    Lint

--- src/constants/production.ts
+++ src/constants/production.ts
@@ -9,6 +9,7 @@ export const firebaseAppId = '1:895779023522:web:b56d399bb3a5601a1c85fb'
 export const notifyEndpoint =
   'https://asia-northeast1-sns-taka3sh-org-157419.cloudfunctions.net/Subscribe'
 
-export const pushEndpoint = 'https://console.cloud.google.com/functions/details/asia-northeast1/Publish?project=sns-taka3sh-org-157419&tab=trigger#:~:text=https%3A//asia-northeast1-sns-taka3sh-org-157419.cloudfunctions.net/Publish'
+export const pushEndpoint =
+  'https://console.cloud.google.com/functions/details/asia-northeast1/Publish?project=sns-taka3sh-org-157419&tab=trigger#:~:text=https%3A//asia-northeast1-sns-taka3sh-org-157419.cloudfunctions.net/Publish'
 
 export const postPrefix = 'posts'
