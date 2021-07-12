4a206c1def7c548e1dd49259476d6b8ae8da88ef
Kaito Udagawa <umireon@gmail.com>
2021-07-13 02:07:48 +0900

    Fix service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,7 +4,7 @@ addEventListener('push', (e) => {
     registration.showNotification(data.notification.title, {
       body: data.notification.body,
       data: data.data,
-      icon: '/icon.png',
+      icon: '/logo192.png',
     })
   )
 })
