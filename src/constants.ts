7c637a252f5321bce4b269e71e2d85c0f408f035
Kaito Udagawa <umireon@gmail.com>
2021-07-26 03:00:44 +0900

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -10,9 +10,9 @@ export const firebaseConfig = {
 }
 
 export const notifyEndpoint =
-  process.env.NODE_ENV === 'production' ? 'https://asia-northeast1-sns-taka3sh-org-157419.cloudfunctions.net/Subscribe' : 'https://asia-northeast1-sns-taka3sh-org-157419.cloudfunctions.net/SubscribeDev'
+  process.env.PULL_REQUEST === 'false' ? 'https://asia-northeast1-sns-taka3sh-org-157419.cloudfunctions.net/Subscribe' : 'https://asia-northeast1-sns-taka3sh-org-157419.cloudfunctions.net/SubscribeDev'
 
 export const pushEndpoint =
-  process.env.NODE_ENV === 'production' ? 'https://asia-northeast1-sns-taka3sh-org-157419.cloudfunctions.net/Publish' : 'https://asia-northeast1-sns-taka3sh-org-157419.cloudfunctions.net/PublishDev'
+  process.env.PULL_REQUEST === 'false' ? 'https://asia-northeast1-sns-taka3sh-org-157419.cloudfunctions.net/Publish' : 'https://asia-northeast1-sns-taka3sh-org-157419.cloudfunctions.net/PublishDev'
 
-export const postPrefix = process.env.NODE_ENV === 'production' ? 'posts' : 'posts-stage'
+export const postPrefix = process.env.PULL_REQUEST === 'false' ? 'posts' : 'posts-stage'
