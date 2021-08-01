cbe87a06aef29c3dcc03b8549f049db17c8837d0
Kaito Udagawa <umireon@gmail.com>
2021-08-01 16:31:43 +0900

    Update cloudfunctions.go

--- cloudfunctions.go
+++ cloudfunctions.go
@@ -1,23 +1,10 @@
 package snstaka3shorg
 
 import (
-	"net/http"
-
 	"github.com/taka3sh/sns.taka3sh.org/packages/api"
 )
 
-func Publish(w http.ResponseWriter, r *http.Request) {
-	api.Publish(w, r)
-}
-
-func PublishDev(w http.ResponseWriter, r *http.Request) {
-	api.PublishDev(w, r)
-}
-
-func Subscribe(w http.ResponseWriter, r *http.Request) {
-	api.Subscribe(w, r)
-}
-
-func SubscribeDev(w http.ResponseWriter, r *http.Request) {
-	api.SubscribeDev(w, r)
-}
+var Publish = api.Publish
+var PublishDev = api.PublishDev
+var Subscribe = api.Subscribe
+var SubscribeDev = api.SubscribeDev
