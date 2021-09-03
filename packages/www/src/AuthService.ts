464eb5a21415e6422693c30ea6274405e751fde6
Kaito Udagawa <umireon@gmail.com>
2021-09-03 16:09:24 +0000

    Fix CreateApptsx

--- packages/www/src/AuthService.ts
+++ packages/www/src/AuthService.ts
@@ -1,25 +1,25 @@
-import firebase from 'firebase/app'
+import { Auth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 
 const AUTHSERVICE_USER = 'AuthService:user'
 
 export const getAuthServiceUser = (): boolean => localStorage.getItem(AUTHSERVICE_USER) === 'loggedIn'
 
 export class AuthService {
-  readonly auth: firebase.auth.Auth
+  readonly auth: Auth
 
-  constructor (auth: firebase.auth.Auth) {
+  constructor (auth: Auth) {
     this.auth = auth
   }
 
   async login (email: string, password: string): Promise<boolean> {
-    const result = await this.auth.signInWithEmailAndPassword(email, password)
+    const result = await signInWithEmailAndPassword(this.auth, email, password)
     if (result.user === null || result.user.email === null) throw new Error('Authentication error')
     localStorage.setItem(AUTHSERVICE_USER, 'loggedIn')
     return true
   }
 
   logout () {
-    this.auth.signOut()
+    signOut(this.auth)
     localStorage.removeItem(AUTHSERVICE_USER)
   }
 }
